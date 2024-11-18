00a561353d01b6a0301dc5a21549f5b076197621
Erwann <erwann.bossuyt@student.uclouvain.be>
2024-11-18 19:17:09 +0100

    tentative de sauvetage de mon travail

--- functions.py
+++ functions.py
@@ -2,7 +2,21 @@ import numpy as np
 import csv as csv
 
 def Dijkstra(C : np.matrix) -> np.matrix:
-    return None
+    mShort=[]
+    rowShort=[]
+    size=C.shape
+    
+    for i in range(size[0]):
+        rowShort=[]
+        for j in range(size[1]):
+            rowShort.append(np.inf)
+        mShort.append(rowShort)
+
+    mShort = np.matrix(mShort)
+
+    mShort[0, 0] = 0
+
+    return mShort
 
 
 def Bellman_Ford(C : np.matrix) -> np.matrix:
@@ -13,9 +27,19 @@ def Floyd_Warshall(C : np.matrix) -> np.matrix:
     return None
 
 
+#main
+matrix=[]
+
 with open('graph.csv', 'r') as f:
-    reader = csv.reader(f, delimiter='\n')
-    for i, line in enumerate(reader):
-        print('line[{}] = {}'.format(i, line))
 
+    reader = csv.reader(f)
+    for line in reader:
+        for i in range(len(line)):
+            line[i]=int(line[i])
+            if line[i] == 1000000000000:
+                line[i] = np.inf
+        matrix.append(line)
 
+m = np.matrix(matrix)
+
+print(Dijkstra(m))
